4f8ae33c29bdc14f31d8e84823fa3c9b85af49f0
joze08 <riosjunior270@gmail.com>
2021-11-25 11:59:57 -0300

    error log2

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -2,15 +2,15 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from "App/Models/User"
 
 export default class AuthController {
-  public async register({ request, /* response */ }: HttpContextContract) {
-    //try {
-    const data = request.only(["email", "password", "curso_id", "nome"])
-    const user = await User.create(data)
-    return user
+  public async register({ request, response }: HttpContextContract) {
+    try {
+      const data = request.only(["email", "password", "curso_id", "nome"])
+      const user = await User.create(data)
+      return user
 
-    /* } catch (error) {
-      response.status(500).send("Erro ao cadastrar usuário!")
-    } */
+    } catch (error) {
+      response.status(500).send(error)
+    }
   }
   public async authenticate({ request, auth, response }: HttpContextContract) {
     try {
